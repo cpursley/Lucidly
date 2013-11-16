28e822d8b00707cdaba2c148b3607422cbc8b99b
Chase Pursley <chasepursley@gmail.com>
2013-11-16 20:03:45 +0000

    Remove admin column from users model as not needed.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131111152439) do
+ActiveRecord::Schema.define(:version => 20131116200113) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
@@ -113,8 +113,8 @@ ActiveRecord::Schema.define(:version => 20131111152439) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "",    :null => false
-    t.string   "encrypted_password",     :default => "",    :null => false
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -126,9 +126,8 @@ ActiveRecord::Schema.define(:version => 20131111152439) do
     t.string   "fullname"
     t.text     "shortbio"
     t.string   "weburl"
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
-    t.boolean  "admin",                  :default => false
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
